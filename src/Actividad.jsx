e3d39c3a35e2c67b72272f691b2d76b31849222f
marquina2014 <luiseduardo15097@gmail.com>
2024-06-27 22:16:57 -0400

    Commit

--- src/Actividad.jsx
+++ src/Actividad.jsx
@@ -37,6 +37,77 @@ function Actividad() {
     handleClose();
   };
 
+  const ticket = [
+    {
+      title: 'ID',
+      value: '33'
+    },
+    {
+      title: 'Fecha Soporte',
+      value: '26/06/2024 10:40'
+    },
+    {
+      title: 'Creado Por',
+      value: 'Gabriel Rondón Barrios'
+    },
+    {
+      title: 'Dashboard',
+      value: 'Control de Horas'
+    },
+    {
+      title: 'Cliente',
+      value: 'BHP'
+    },
+    {
+      title: 'Horas Totales',
+      value: '25'
+    },
+    {
+      title: 'Observaciones Generales',
+      value: 'asd'
+    },
+    {
+      title: 'Adjuntos',
+      value: 'Sin Archivos Cargados'
+    }
+  ]
+  const Actividad = [
+    
+    {
+      title: 'ID',
+      value: '33'
+    },
+    {
+      title: 'Horas Reales',
+      value: '15'
+    },
+    {
+      title: 'Solicitado Por',
+      value: 'Gabriel Rondón Barrios'
+    },
+    {
+      title: 'Facturable',
+      value: 'No'
+    },
+    {
+      title: 'Detalles',
+      value: 'asd'
+    },
+    {
+      title: 'Horas Estimadas',
+      value: '7'
+    },
+    {
+      title: 'Asignado A',
+      value: 'Gabriel Rondón'
+    },
+    {
+      title: 'Observaciones Consultor',
+      value: 'asd'
+    }
+    
+  ]
+
   return (
     <div className="approval-container">
       <div className="header">
@@ -48,16 +119,14 @@ function Actividad() {
           <div className="section-content">
             <div className="row">
               <div className="column">
-                <div>ID: 33</div>
-                <div>Fecha Soporte: 26/06/2024 10:40</div>
-                <div>Creado Por: Gabriel Rondón Barrios</div>
-                <div>Dashboard: Control de Horas</div>
-              </div>
-              <div className="column">
-                <div>Cliente: BHP</div>
-                <div>Horas Totales: 25</div>
-                <div>Observaciones Generales: asd</div>
-                <div>Adjuntos: Sin Archivos Cargados</div>
+                {
+                  ticket.map(item=>
+                    <div>
+                      <div className='inputLabel'>{item.title}</div>
+                      <div className='inputValue'>{item.value}</div>
+                    </div>
+                  )
+                }
               </div>
             </div>
           </div>
@@ -66,31 +135,30 @@ function Actividad() {
           <h3 className="TicAT-Titulo">Actividad Realizada</h3>
           <div className="section-content">
             <div className="row">
-              <div className="column">
-                <div>ID: 45</div>
-                <div>Horas Reales: 15</div>
-                <div>Solicitado Por: Gabriel Rondón Barrios</div>
-                <div>Facturable: No</div>
-                <div>Detalles: asd</div>
-              </div>
-              <div className="column">
-                <div>Horas Estimadas: 7</div>
-                <div>Asignado A: Gabriel Rondón</div>
-                <div>Observaciones Consultor: asd</div>
-                <div>Adjuntos: Sin Archivos Cargados</div>
+            <div className="column">
+            {
+                  Actividad.map(item=>
+                    <div>
+                      <div className='inputLabel'>{item.title}</div>
+                      <div className='inputValue'>{item.value}</div>
+                    </div>
+                  )
+                }
               </div>
             </div>
           </div>
         </div>
-        <div className="buttons">
-          <button className="btn btn-danger btn-lg mr-2" onClick={() => handleShow('Rechazar Actividad')}>
-            Rechazar
-          </button>
-          <button className="btn btn-success btn-lg" onClick={() => handleShow('Aprobar Actividad')}>
-            Aprobar
-          </button>
-        </div>
+        
       </div>
+      <div className="buttons">
+        <button className="btnAproval btn btn-danger btn-lg mr-2" onClick={() => handleShow('Rechazar Actividad')}>
+          Rechazar
+        </button>
+        <button className="btnAproval btn btn-success btn-lg" onClick={() => handleShow('Aprobar Actividad')}>
+          Aprobar
+        </button>
+      </div>
+      
 
       <Modal show={modalShow} onHide={handleClose} centered>
         <motion.div initial={{ rotateY: 90 }} animate={{ rotateY: 0 }} transition={{ duration: 0.5 }}>
@@ -111,18 +179,20 @@ function Actividad() {
           </Modal.Body>
           <Modal.Footer>
             {modalTitle === 'Rechazar Actividad' && (
-              <Button variant="danger" onClick={handleReject}>
+              <button onClick={handleReject} className=" btn btn-danger btn-lg mr-2" >
                 Rechazar
-              </Button>
+              </button>
+              
             )}
              {modalTitle === 'Aprobar Actividad' && (
-              <Button variant="success" onClick={handleApprove}>
+              <button onClick={handleApprove} className=" btn btn-success btn-lg">
                 Aprobar
-              </Button>
+              </button>
             )}
-            <Button variant="secondary" onClick={handleClose}>
+            <button onClick={handleClose} className="btn btn-cerrar btn-lg">
               Cerrar
-            </Button>
+            </button>
+            
           </Modal.Footer>
         </motion.div>
       </Modal>
